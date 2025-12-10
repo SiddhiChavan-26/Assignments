41dddf1c210c812fdc46bbc1490023ed5270b60b
Siddhi Chavan <siddhichavan073@gmail.com>
2025-12-10 17:13:52 +0530

    rectangle geometry

---
+++ geometry.py
@@ -0,0 +1,6 @@
+def calc_rect_area(len, br):
+    print("Rect area : ", len * br)
+
+def calc_rect_peri(len, br):
+    print("Perimeter of rectangle : ",2 * (len + br))
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import arithmetic
+import geometry as geo
+from geometry import calc_rect_area
 
 print("Hello world!!")
 a = int(input("Enter first number : "))
@@ -6,3 +8,8 @@ b = int (input("Enter second number: "))
 
 arithmetic.add(a, b)
 arithmetic.subtract(a, b)
+
+length = int(input("Enter length: "))
+breadth = int(input("Enter breadth : "))
+
+geo.calc_rect_area(length, breadth)
